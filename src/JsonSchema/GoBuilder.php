8029e35b7da2e3b8754ce085d9f45108114dc20c
vpoturaev <viacheslav.poturaev@lazada.com>
2017-08-22 10:34:34 +0700

    loose swagger validation

---
+++ src/JsonSchema/GoBuilder.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Swaggest\GoCodeBuilder\JsonSchema;
+
+use Swaggest\JsonSchema\JsonSchema;
+use Swaggest\JsonSchema\Schema;
+use Swaggest\PhpCodeBuilder\PhpAnyType;
+use Swaggest\PhpCodeBuilder\PhpClass;
+use Swaggest\PhpCodeBuilder\PhpClassProperty;
+use Swaggest\PhpCodeBuilder\PhpFlags;
+use Swaggest\PhpCodeBuilder\PhpFunction;
+use Swaggest\PhpCodeBuilder\PhpNamedVar;
+use Swaggest\PhpCodeBuilder\PhpCode;
+use Swaggest\PhpCodeBuilder\Property\Getter;
+use Swaggest\PhpCodeBuilder\Property\Setter;
+
+/**
+ * @todo properly process $ref, $schema property names
+ */
+class GoBuilder
+{
+    /** @var \SplObjectStorage|GeneratedClass[] */
+    private $generatedClasses;
+    private $untitledIndex = 0;
+
+    public function __construct()
+    {
+        $this->generatedClasses = new \SplObjectStorage();
+    }
+
+    public $buildGetters = false;
+    public $buildSetters = false;
+
+    /**
+     * @param JsonSchema $schema
+     * @param string $path
+     * @return PhpAnyType
+     */
+    public function getType(JsonSchema $schema, $path = '#')
+    {
+        $typeBuilder = new TypeBuilder($schema, $path, $this);
+        return $typeBuilder->build();
+    }
+
+
+    public function getClass(JsonSchema $schema, $path)
+    {
+        if ($this->generatedClasses->contains($schema)) {
+            return $this->generatedClasses[$schema]->class;
+        } else {
+            return $this->makeClass($schema, $path)->class;
+        }
+    }
+
+    private function makeClass(Schema $schema, $path)
+    {
+        if (empty($path)) {
+            throw new Exception('Empty path');
+        }
+        $generatedClass = new GeneratedClass();
+        $generatedClass->schema = $schema;
+
+        $class = new PhpClass();
+        $class->setName('Untitled' . ++$this->untitledIndex);
+        $class->setExtends(Palette::classStructureClass());
+
+
+        $setupProperties = new PhpFunction('setUpProperties');
+        $setupProperties
+            ->setVisibility(PhpFlags::VIS_PUBLIC)
+            ->setIsStatic(true);
+        $setupProperties
+            ->addArgument(new PhpNamedVar('properties', Palette::propertiesOrStaticClass()))
+            ->addArgument(new PhpNamedVar('ownerSchema', Palette::schemaClass()));
+
+        $body = new PhpCode();
+
+        $class->addMethod($setupProperties);
+
+        $generatedClass->class = $class;
+        $generatedClass->path = $path;
+
+        $this->generatedClasses->attach($schema, $generatedClass);
+
+        foreach ($schema->properties as $name => $property) {
+            $propertyName = PhpCode::makePhpName($name);
+
+            $schemaBuilder = new SchemaBuilder($property, '$properties->' . $propertyName, $path . '->' . $name, $this);
+            $phpProperty = new PhpClassProperty($propertyName, $this->getType($property, $path . '->' . $name));
+            if ($property->description) {
+                $phpProperty->setDescription($property->description);
+            }
+            $class->addProperty($phpProperty);
+            if ($this->buildGetters) {
+                $class->addMethod(new Getter($phpProperty));
+            }
+            if ($this->buildSetters) {
+                $class->addMethod(new Setter($phpProperty));
+            }
+            $body->addSnippet(
+                $schemaBuilder->build()
+            );
+            if ($propertyName != $name) {
+                $body->addSnippet('$ownerSchema->addPropertyMapping(' . var_export($name, 1) . ', self::names()->'
+                    . $propertyName . ");\n");
+            }
+
+            $class->addMethod(new Setter($phpProperty, true));
+        }
+
+        $schemaBuilder = new SchemaBuilder($schema, '$ownerSchema', $path, $this);
+        $schemaBuilder->setSkipProperties(true);
+        $body->addSnippet($schemaBuilder->build());
+
+        $setupProperties->setBody($body);
+
+        return $generatedClass;
+    }
+
+    /**
+     * @return GeneratedClass[]
+     */
+    public function getGeneratedClasses()
+    {
+        $result = array();
+        foreach ($this->generatedClasses as $schema) {
+            $result[] = $this->generatedClasses[$schema];
+        }
+        return $result;
+    }
+
+}
---
+++ src/JsonSchema/TypeBuilder.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Swaggest\GoCodeBuilder\JsonSchema;
+
+
+use Swaggest\JsonSchema\Constraint\Type;
+use Swaggest\JsonSchema\JsonSchema;
+use Swaggest\JsonSchema\Schema;
+use Swaggest\PhpCodeBuilder\PhpStdType;
+use Swaggest\PhpCodeBuilder\Types\ArrayOf;
+use Swaggest\PhpCodeBuilder\Types\OrType;
+
+class TypeBuilder
+{
+    /** @var JsonSchema */
+    private $schema;
+    /** @var string */
+    private $path;
+    /** @var GoBuilder */
+    private $goBuilder;
+    /** @var OrType */
+    private $result;
+
+    /**
+     * TypeBuilder constructor.
+     * @param Schema $schema
+     * @param $path
+     * @param GoBuilder $goBuilder
+     */
+    public function __construct(JsonSchema $schema, $path, GoBuilder $goBuilder)
+    {
+        $this->schema = $schema;
+        $this->path = $path;
+        $this->goBuilder = $goBuilder;
+    }
+
+    private function processLogicType()
+    {
+        $orSchemas = null;
+        if ($this->schema->allOf !== null) {
+            $orSchemas = $this->schema->allOf;
+        } elseif ($this->schema->anyOf !== null) {
+            $orSchemas = $this->schema->anyOf;
+        } elseif ($this->schema->oneOf !== null) {
+            $orSchemas = $this->schema->oneOf;
+        }
+
+        if ($orSchemas !== null) {
+            foreach ($orSchemas as $item) {
+                $this->result->add($this->goBuilder->getType($item, $this->path));
+            }
+        }
+    }
+
+    private function processArrayType()
+    {
+        $schema = $this->schema;
+
+        $pathItems = (string)Schema::names()->items;
+        if ($schema->items instanceof Schema) {
+            $items = array();
+            $additionalItems = $schema->items;
+        } elseif ($schema->items === null) { // items defaults to empty schema so everything is valid
+            $items = array();
+            $additionalItems = true;
+        } else { // listed items
+            $items = $schema->items;
+            $additionalItems = $schema->additionalItems;
+            $pathItems = (string)Schema::names()->additionalItems;
+        }
+
+        if ($items !== null || $additionalItems !== null) {
+            $itemsLen = is_array($items) ? count($items) : 0;
+            $index = 0;
+            if ($index < $itemsLen) {
+            } else {
+                if ($additionalItems instanceof Schema) {
+                    $this->result->add(new ArrayOf($this->goBuilder->getType($additionalItems, $this->path . '->' . $pathItems)));
+                }
+            }
+        }
+    }
+
+    private function processObjectType()
+    {
+        if ($this->schema->patternProperties !== null) {
+            foreach ($this->schema->patternProperties as $pattern => $schema) {
+                $this->result->add(new ArrayOf($this->goBuilder->getType($schema, $this->path . '->' . $pattern)));
+            }
+        }
+
+        if ($this->schema->additionalProperties instanceof Schema) {
+            $this->result->add(new ArrayOf($this->goBuilder->getType(
+                $this->schema->additionalProperties,
+                $this->path . '->' . (string)Schema::names()->additionalProperties)
+            ));
+        }
+    }
+
+    private function typeSwitch($type)
+    {
+        switch ($type) {
+            case Type::INTEGER:
+                return PhpStdType::int();
+
+            case Type::NUMBER:
+                return PhpStdType::float();
+
+            case TYPE::BOOLEAN:
+                return PhpStdType::bool();
+
+            case Type::STRING:
+                return PhpStdType::string();
+
+            /*
+            case Type::OBJECT:
+                return PhpStdType::object();*/
+
+            case Type::ARR:
+                return PhpStdType::arr();
+
+            case Type::NULL:
+                return PhpStdType::null();
+
+            default:
+                return null;
+        }
+    }
+
+    private function processNamedClass(JsonSchema $schema, $path)
+    {
+        if ($schema->properties !== null) {
+            $class = $this->goBuilder->getClass($schema, $path);
+            $this->result->add($class);
+        }
+    }
+
+    /**
+     * @return OrType
+     */
+    public function build()
+    {
+        $schema = $this->schema;
+
+        $this->result = new OrType();
+
+        if (null !== $path = $this->schema->getFromRef()) {
+            $this->path = $this->schema->getFromRef();
+            //$this->result->add($this->phpBuilder->getType($this->schema->ref->getData(), $this->schema->ref->ref));
+        }
+
+
+        $this->processNamedClass($this->schema, $this->path);
+        $this->processLogicType();
+        $this->processArrayType();
+        $this->processObjectType();
+
+        if (is_array($this->schema->type)) {
+            foreach ($this->schema->type as $type) {
+                $this->result->add($this->typeSwitch($type));
+            }
+        } elseif ($this->schema->type) {
+            $this->result->add($this->typeSwitch($this->schema->type));
+        }
+
+        return $this->result;
+
+    }
+}
